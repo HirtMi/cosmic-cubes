e55572f0666f8f721e4bfd7237640a311df08945
HirtMi <101597200+HirtMi@users.noreply.github.com>
2022-07-31 15:52:28 -0400

    Update geometry.js
    Move object functions to prototypes

--- src/geometry.js
+++ src/geometry.js
@@ -5,31 +5,34 @@ function Point(x, y, z){
     this.x = x;
     this.y = y;
     this.z = z;
-
-    this.rotateZ = function(angleZ, centerOfRotation){
-        let radsZ = (Math.PI / 180) * angleZ;
-        let px = this.x;
-        let py = this.y;
-        this.x = ((px - centerOfRotation.x) * Math.cos(radsZ) - (py - centerOfRotation.y) * Math.sin(radsZ)) + centerOfRotation.x;
-        this.y = ((px - centerOfRotation.x) * Math.sin(radsZ) + (py - centerOfRotation.y) * Math.cos(radsZ)) + centerOfRotation.y;
-    }
-    
-    this.rotateX = function(angleX, centerOfRotation){
-        let radsX = (Math.PI / 180) * angleX;
-        let py = this.y;
-        let pz = this.z;
-        this.y = ((py - centerOfRotation.y) * Math.cos(radsX) - (pz - centerOfRotation.z) * Math.sin(radsX)) + centerOfRotation.y;
-        this.z = ((py - centerOfRotation.y) * Math.sin(radsX) + (pz - centerOfRotation.z) * Math.cos(radsX)) + centerOfRotation.z;
-    }
-    
-    this.rotateY = function(angleY, centerOfRotation){
-        let radsY = (Math.PI / 180) * angleY;
-        let px = this.x;
-        let pz = this.z;
-        this.x = ((px - centerOfRotation.x) * Math.cos(radsY) + (pz - centerOfRotation.z) * Math.sin(radsY)) + centerOfRotation.x;
-        this.z = (-1 * (px - centerOfRotation.x) * Math.sin(radsY) + (pz - centerOfRotation.z) * Math.cos(radsY)) + centerOfRotation.z;
-    }
 }
+
+
+Point.prototype.rotateZ = function(angleZ, centerOfRotation){
+    let radsZ = (Math.PI / 180) * angleZ;
+    let px = this.x;
+    let py = this.y;
+    this.x = ((px - centerOfRotation.x) * Math.cos(radsZ) - (py - centerOfRotation.y) * Math.sin(radsZ)) + centerOfRotation.x;
+    this.y = ((px - centerOfRotation.x) * Math.sin(radsZ) + (py - centerOfRotation.y) * Math.cos(radsZ)) + centerOfRotation.y;
+}
+
+
+Point.prototype.rotateX = function(angleX, centerOfRotation){
+    let radsX = (Math.PI / 180) * angleX;
+    let py = this.y;
+    let pz = this.z;
+    this.y = ((py - centerOfRotation.y) * Math.cos(radsX) - (pz - centerOfRotation.z) * Math.sin(radsX)) + centerOfRotation.y;
+    this.z = ((py - centerOfRotation.y) * Math.sin(radsX) + (pz - centerOfRotation.z) * Math.cos(radsX)) + centerOfRotation.z;
+}
+
+
+Point.prototype.rotateY = function(angleY, centerOfRotation){
+    let radsY = (Math.PI / 180) * angleY;
+    let px = this.x;
+    let pz = this.z;
+    this.x = ((px - centerOfRotation.x) * Math.cos(radsY) + (pz - centerOfRotation.z) * Math.sin(radsY)) + centerOfRotation.x;
+    this.z = (-1 * (px - centerOfRotation.x) * Math.sin(radsY) + (pz - centerOfRotation.z) * Math.cos(radsY)) + centerOfRotation.z;
+}           
 
 
 function Cube(x, y, z, size){
@@ -56,72 +59,80 @@ function Cube(x, y, z, size){
         [this.vertices[7], this.vertices[0], this.vertices[3], this.vertices[4]],
         [this.vertices[1], this.vertices[6], this.vertices[5], this.vertices[2]]
     ];
-  
-    this.drawFrame = function(){
-        if (this.center.z < -2000){return;}
-        for (let i = 0; i < this.faces.length; i++){
-            for (let j = 0; j < this.faces[i].length - 1; j++){
-                connectPoints(this.faces[i][j], this.faces[i][j+1]);
-                }
-            connectPoints(this.faces[i][0], this.faces[i][this.faces[i].length - 1]);
+}
+
+
+Cube.prototype.drawFrame = function(){
+    if (this.center.z < -2000){return;}
+    for (let i = 0; i < this.faces.length; i++){
+        for (let j = 0; j < this.faces[i].length - 1; j++){
+            connectPoints(this.faces[i][j], this.faces[i][j+1]);
             }
-        }
-
-    this.connectVerticesToOrigin = function(){
-        for (let i = 0; i < 8; i++){
-            connectPoints(this.vertices[i], this.center);
-        }
+        connectPoints(this.faces[i][0], this.faces[i][this.faces[i].length - 1]);
     }
+}
 
-    this.rotateZ = function(angle){
-        for (let i = 0; i < this.vertices.length; i++){
-            this.vertices[i].rotateZ(angle, this.center);
-        }
-    }
 
-    this.rotateX = function(angle){
-        for (let i = 0; i < this.vertices.length; i++){
-            this.vertices[i].rotateX(angle, this.center);
-        }
+Cube.prototype.connectVerticesToOrigin = function(){
+    for (let i = 0; i < 8; i++){
+        connectPoints(this.vertices[i], this.center);
     }
+}
 
-    this.rotateY = function(angle){
-        for (let i = 0; i < this.vertices.length; i++){
-            this.vertices[i].rotateY(angle, this.center);
-        }
-    }
 
-    this.rotate = function(angleZ, angleX, angleY){
-        if (angleZ != 0){this.rotateZ(angleZ);}
-        if (angleX != 0){this.rotateX(angleX);}
-        if (angleY != 0){this.rotateY(angleY);}
+Cube.prototype.rotateZ = function(angle){
+    for (let i = 0; i < this.vertices.length; i++){
+        this.vertices[i].rotateZ(angle, this.center);
     }
+}
 
-    this.translate = function(dx, dy, dz){
-        this.center.x += dx;
-        this.center.y += dy;
-        this.center.z += dz;
-        for (let i = 0; i < this.vertices.length; i++){
-            this.vertices[i].x += dx;
-            this.vertices[i].y += dy;
-            this.vertices[i].z += dz;
-        }
-        if (dz != 0){
-            this.scale(1 - (.001 * dz));
-        }
-    }
 
-    this.scale = function(scalar){
-        for (let i = 0; i < this.vertices.length; i++){
-            this.scalePoint(this.vertices[i], this.center, scalar)
-        }
+Cube.prototype.rotateX = function(angle){
+    for (let i = 0; i < this.vertices.length; i++){
+        this.vertices[i].rotateX(angle, this.center);
     }
+}
 
-    this.scalePoint = function(point, center, scalar){
-        point.x = center.x + scalar * (point.x - center.x);
-        point.y = center.y + scalar * (point.y - center.y);
-        point.z = center.z + scalar * (point.z - center.z);
+Cube.prototype.rotateY = function(angle){
+    for (let i = 0; i < this.vertices.length; i++){
+        this.vertices[i].rotateY(angle, this.center);
     }
+}
+
+
+Cube.prototype.rotate = function(angleZ, angleX, angleY){
+    if (angleZ != 0){this.rotateZ(angleZ);}
+    if (angleX != 0){this.rotateX(angleX);}
+    if (angleY != 0){this.rotateY(angleY);}
+}
+
+
+Cube.prototype.translate = function(dx, dy, dz){
+    this.center.x += dx;
+    this.center.y += dy;
+    this.center.z += dz;
+    for (let i = 0; i < this.vertices.length; i++){
+        this.vertices[i].x += dx;
+        this.vertices[i].y += dy;
+        this.vertices[i].z += dz;
+    }
+    if (dz != 0){
+        this.scale(1 - (.001 * dz));
+    }
+}
+
+
+Cube.prototype.scale = function(scalar){
+    for (let i = 0; i < this.vertices.length; i++){
+        this.scalePoint(this.vertices[i], this.center, scalar)
+    }
+}
+
+
+Cube.prototype.scalePoint = function(point, center, scalar){
+    point.x = center.x + scalar * (point.x - center.x);
+    point.y = center.y + scalar * (point.y - center.y);
+    point.z = center.z + scalar * (point.z - center.z);
 }
 
 
